ffe7ce38fb401079cdb6c4de672b5d09a78ddc4d
Nurbaki Vural <nurbakivural@gmail.com>
2022-10-29 17:27:49 +0300

    added sidebar

--- shareme_frontend/src/components/Sidebar.jsx
+++ shareme_frontend/src/components/Sidebar.jsx
@@ -1,7 +1,50 @@
 import React from "react"
+import { NavLink, Link } from "react-router-dom"
+// import { RiHome2Fill } from "react-icons/ri"
+// import { IoArrowForward } from "react-icons/io5"
 
-const Sidebar = () => {
-  return <div>Sidebar</div>
+// import { MdHome, MdKeyboardArrowRight } from "react-icons/md"
+import { AiFillCaretRight, AiFillHome } from "react-icons/ai"
+
+import logo from "../assets/logo.png"
+
+const isNotActiveStyle =
+  "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize"
+const isActiveStyle =
+  "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize"
+
+const Sidebar = ({ user, closeToggle }) => {
+  const handleCloseSidebar = () => {
+    if (closeToggle) closeToggle(false)
+  }
+
+  return (
+    <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
+      <div className="flex flex-col">
+        <Link
+          to="/"
+          className="flex px-5 gap-2 my-6 pt-1 w-190 items-center"
+          onClick={handleCloseSidebar}
+        >
+          <img src={logo} className="w-full" alt="logo" />
+        </Link>
+        <div className="flex flex-col gap-5">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? isActiveStyle : isNotActiveStyle
+            }
+          >
+            <AiFillHome />
+            Home
+          </NavLink>
+          <h3 className="mt-2 px-5 text-base 2xl:text-xl">
+            Discover Categories
+          </h3>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
